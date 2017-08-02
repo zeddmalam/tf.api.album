cb93bb37d4340f44b3b220b21a37d28f40d18d34
Zedd <zedd@Kardans-MacBook-Pro-2.local>
2017-08-02 18:54:13 +0200

    id processing

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ const neo4j = require('neo4j-driver').v1;
 
 exports.handler = (event, context, callback) => {
   const ENV = context.invokedFunctionArn.split(':').slice(-1).pop();
-  let driver = neo4j.driver(process.env[ENV + '_DB_BLOT'], neo4j.auth.basic(process.env[ENV + '_DB_USER'], process.env[ENV + '_DB_PASSWORD']));
+  let driver = neo4j.driver(process.env[ENV + '_DB_BOLT'], neo4j.auth.basic(process.env[ENV + '_DB_USER'], process.env[ENV + '_DB_PASSWORD']));
   let session = driver.session();
   
   if('LIST' === event.method){
     console.time('query duration');
     session
-      .run("MATCH (n:ALBUM) RETURN n SKIP 0 LIMIT 10")
+      .run("MATCH (n:Album) RETURN n SKIP 0 LIMIT 10")
       .then(result => {
         let data = [];
         result.records.forEach(record => {
-          data.push(record._fields);
+          record._fields[0].identity = neo4j.int(record._fields[0].identity).toNumber();
+          data.push(record._fields[0]);
         });
         session.close();
         console.timeEnd('query duration');
         return context.succeed(data);
       })
       .catch(error => {
-        console.log(error);
+        console.log('ERROR', error);
         session.close();
         console.timeEnd('query duration');
         return context.fail(error);
